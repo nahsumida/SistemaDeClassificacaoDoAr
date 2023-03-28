27ef2fa4cfa13d204a77d48945aab578066240d5
Natália Naomi Sumida <75488386+nahsumida@users.noreply.github.com>
2023-03-28 09:16:15 -0300

    use cases

--- PI_Grupo6.py
+++ PI_Grupo6.py
@@ -17,9 +17,18 @@ so2 = -1
 while so2 < 0: 
 	so2 = float(input("Digite o nivel de dioxido de enxofre: "))
 	
-	
-	
 qldAr = "" 
+
+if mp10 > 250 or mp2.5 > 125 or o3 > 200 or co > 15 or no2 > 1130 or so2 > 800: 
+	qldAr = "Pessima" 
+elif 150 > mp10 and mp10 < 250 or 75 > mp2.5 and mp2.5 < 125 or 160 > o3 and o3 < 200 or 13 > co and co < 15  or 320 > no2 and no2 < 1130 or 365 > so2 and so2 < 800: 
+	qldAr = "Muito Ruim"
+elif 100 > mp10 and mp10 < 150 or 50 > mp2.5 and mp2.5 < 75 or 130 > o3 and o3 < 160 or 11 > co and co < 13  or 240 > no2 and no2 < 320 or 40 > so2 and so2 < 365: 
+	qldAr = "Ruim"
+elif 50 > mp10 and mp10 < 100 or 25 > mp2.5 and mp2.5 < 50 or 100 > o3 and o3 < 130 or 9 > co and co < 11 or 200 > no2 and no2 < 240 or 20 > so2 and so2 < 40:  
+	qldAr =	"Moderado"
+else: 
+	qldAr = "Bom"
 
 if qldAr == "Bom": 
 	print("Qualidade do ar:", qldAr, "\nImplicacoes a saude: Nenhuma"
