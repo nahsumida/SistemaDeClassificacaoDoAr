c90d585a1c988f4130888df40392d4ccc5637317
Natália Naomi Sumida <75488386+nahsumida@users.noreply.github.com>
2023-03-28 09:01:41 -0300

    output

--- PI_Grupo6.py
+++ PI_Grupo6.py
@@ -17,3 +17,17 @@ so2 = -1
 while so2 < 0: 
 	so2 = float(input("Digite o nivel de dioxido de enxofre: "))
 	
+	
+	
+qldAr = "" 
+
+if qldAr == "Bom": 
+	print("Qualidade do ar:", qldAr, "\nImplicacoes a saude: Nenhuma"
+elif qldAr == "Moderada": 
+	print("Qualidade do ar:", qldAr, "\nImplicacoes a saude: Pessoas de grupos sensíveis (crianças, idosos e pessoas com doenças respiratórias e cardíacas) podem apresentar sintomas como tosse seca e cansaço. A população, em geral, não é afetada.")
+elif qldAr == "Ruim": 
+	print("Qualidade do ar:", qldAr, "\nImplicacoes a saude: Toda a população pode apresentar sintomas como tosse seca, cansaço, ardor nos olhos, nariz e garganta. Pessoas de grupos sensíveis (crianças, idosos e pessoas com doenças respiratórias e cardíacas) podem apresentar efeitos mais sérios na saúde.")
+elif qldAr == "Muito Ruim":
+ 	print("Qualidade do ar:", qldAr, "\nImplicacoes a saude: Toda a população pode apresentar sintomas como tosse seca, cansaço, ardor nos olhos, nariz e garganta e ainda falta de ar e respiração ofegante. Efeitos mais graves à saúde de grupos sensíveis.")
+else: 
+	print("Qualidade do ar:", qldAr, "\nImplicacoes a saude: Toda a população pode apresentar sérios riscos de manifestações de doenças respiratórias e cardiovasculares. Aumento de mortes prematuras em pessoas de grupos sensíveis."
