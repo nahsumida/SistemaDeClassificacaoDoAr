80ef19ad96a09bacb00102555ec9c1ba96023041
Natália Naomi Sumida <75488386+nahsumida@users.noreply.github.com>
2023-03-28 08:49:20 -0300

    input while values are minor than 0

--- PI_Grupo6.py
+++ PI_Grupo6.py
@@ -1,7 +1,19 @@
-mp10 = float(input(""))
-mp2.5 = 
-o3 = 
-co = 
-no2 =
-so2 = 
-
+mp10 = -1
+while mp10 < 0: 
+	mp10 = float(input("Digite a quantidade de particulas inalaveis: "))
+mp2.5 = -1
+while mp2.5 < 0: 
+	mp2.5 = float(input("Digite a quantidade de particulas inalaveis finas: "))
+o3 = -1
+while o3 < 0: 
+	o3 = float(input("Digite o nivel de ozonio: "))
+co = -1
+while co < 0: 
+	co = float(input("Digite o nivel de monoxido de carbono: "))
+no2 = -1
+while no2 < 0: 
+	no2 = float(input("Digite o nivel de dioxido de nitrogenio: "))
+so2 = -1
+while so2 < 0: 
+	so2 = float(input("Digite o nivel de dioxido de enxofre: "))
+	
